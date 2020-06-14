1c06681fe2780a04ca9e563da2846db95acec1ed
Simon Rowe <simon@wiremoons.com>
2020-06-14 10:06:39 +0100

    Minor improvements to comments

--- src/weather.nim
+++ src/weather.nim
@@ -5,49 +5,24 @@
 ## Created by Simon Rowe <simon@wiremoons.com> on 03 Nov 2019
 ## Source code available from GitHub: https://github.com/wiremoons/weather.git
 ## 
-## Weather forecast retrieval tool that uses the DarkSky API and weather data
+## Weather forecast retrieval tool that uses the DarkSky API and weather data.
 ##
-## MIT License
-## Copyright (c) 2020 Simon Rowe
-##
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-#
+## MIT License. Copyright (c) 2020 Simon Rowe
 
-# import required Nim standard libraries
 import os, strformat, times
-# import source code from our own files
+
+# import local source code
 import getdata, settings, version, help, weatherOutput, types, dbgUtils,
   getgeoloc, yesno, getplace
 
-#///////////////////////////////////////////////////////////////
-#                      MAIN START
-#///////////////////////////////////////////////////////////////
-
-# JSON packages parse:
-# https://forum.nim-lang.org/t/5730
-
 # create a new weather object - see 'types.nim'
 var weather = Weather()
+
 # default is not to offer to lookup a new location
 var needPlace = false
 
+# check for command line options use
 let args = commandLineParams()
-
-# check if the user wanted any command line options
 if paramCount() > 0:
   case args[0]
   of "-h", "--help":
@@ -60,17 +35,15 @@ if paramCount() > 0:
     echo "Unknown command line parameter given - see options below:"
     showHelp()
 
-# get app settings if they exist - if not create them with default version...
+# get app settings or create default version - see 'settings.nim'
 while not getSettings(weather):
   createDefaultSettings()
-  # above created a new 'default' settings file so also add a location too?
-  #needPlace = true #<- disabled so apps runs without prompt by default
   echo fmt"""
 
-    NOTE: a default configuration has been added to the settings file...
+    NOTE: a default configuration has been created.
      
-    Re-run as:  '{extractFilename(getAppFilename())} -p'  to re-configure.
-    This command lets you choose your own preferred weather forecast location.
+    Use:  '{extractFilename(getAppFilename())} -p'  to re-configure.
+    
     See output of: '{extractFilename(getAppFilename())} -h' for more information.
     """
 
@@ -89,25 +62,31 @@ if weather.darkskyKey == "":
          Add your key to the settings file, or via the environment as:
     
              export DSAPI="Your_DarkSky_API_Key_Here"
+              
+                              or
+              
+              $env:DSAPI="Your_DarkSky_API_Key_Here"
     
          Unable to continue - program exiting...
     """
     debug fmt"ABORT: no stored or provided DarkSky API key found: '{weather.darkskyKey}'" 
     quit (100)
+  else:
+    echo "DarkSky API key found. Will be added to the settings file."
 
 debug fmt"Stored DarkSky API key is: '{weather.darkskyKey}'"
 
 
 # Obtain Geo Location data: try Google API first..
 # If no key in the settings or in env variable 'GAPI' the
-# program will fallback to OpenStreet Map when needed..
+# program will fallback to OpenStreet Map if needed
 #
 # If no Google API exists from settings - try from env variable 'GAPI':
 if weather.googleKey == "":
   weather.googleKey = getEnv("GAPI", "")
 debug fmt"Any stored Google API is: '{weather.googleKey}'"
 
-# check if there is a reason to setup a new location
+# check if there is a reason to setup a new geographical forecast location
 if needPlace and getYesNo("Would you like to add your weather forecast location?"):
   debug "User request the addition of a new location..."
 
