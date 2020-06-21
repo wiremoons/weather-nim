26e49b7ae9bd898127dfb8f6bde089ea23afa6d2
Simon Rowe <simon@wiremoons.com>
2020-06-21 10:01:53 +0100

    Refined code comments and reduced header info banner

--- src/yesno.nim
+++ src/yesno.nim
@@ -1,54 +1,39 @@
-## Source code available from GitHub: https://github.com/wiremoons/weather.git
 ##
 ## SOURCE FILE: yesno.nim
 ## 
-## Weather forecast retrieval tool that uses the DarkSky API and weather data
-##
 ## MIT License
-## Copyright (c) 2019 Simon Rowe
+## Copyright (c) 2020 Simon Rowe
+## https://github.com/wiremoons/
 ##
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-#
 
-# import the Nim language modules
+# import the required Nim language modules
 import strformat, terminal
 
 proc getYesNo*(question: string): bool =
   ##
   ## PROCEDURE: getYesNo
   ## Input: a string containing the question to ask the user
-  ## Returns: a boolean where: true == yes || false == no
+  ## Returns: a boolean where user response is: true == yes || false == no
   ## Description: the question string is printed to the screen so the
-  ## user can respond with a 'yes' or 'no' reponse. Accepts different
-  ## forms of 'yes' or 'no' to suit the user
+  ## user can respond with a 'yes' or 'no' response. Accepts different
+  ## forms of 'yes' or 'no' to suit the user prefered approach to 
+  ## answering.
   ##
 
   # reset terminal to defaults on program exit
   system.addQuitProc(resetAttributes)
 
-  # output question to the user:
+  # output provided question to the user:
   write(stdout, "\n » QUESTION: ")
   stdout.styledWrite(fgCyan, fmt"{question} ", resetStyle)
   write(stdout, " [ Y/N ] : ")
 
+  # read user input until a reponse matching a form of 'y' or 'n' is
+  # obtained. Print an error on any unmatched user inputs.
   while true:
     case readLine(stdin)
-      of "y", "Y", "yes", "Yes": return true
-      of "n", "N", "no", "No": return false
+      of "y", "Y", "yes", "Yes", "YES": return true
+      of "n", "N", "no", "No", "NO": return false
       else:
         stdout.styledWrite(fgRed, "ERROR: ", resetStyle)
         write(stdout, "please enter 'y' or 'n' : ")
