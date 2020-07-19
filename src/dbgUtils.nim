9a8e5b2a810db15d1de4c08c088b0179c5fecf76
Simon Rowe <simon@wiremoons.com>
2020-07-19 09:46:37 +0100

    Corrected spelling errors in comments

--- src/dbgUtils.nim
+++ src/dbgUtils.nim
@@ -9,14 +9,14 @@
 template debug*(data: untyped) =
   ##
   ## PROCEDURE: debug
-  ## Input: any debug message to include with the ouput
+  ## Input: any debug message to include with the output
   ## Returns: nothing
   ## Description: a Nim template used to output debug messages
   ## from the program. Is not used when a program is compiled
   ## as a 'release' version - allowing automatic disabling of
   ## debug output for final application builds. Output includes
-  ## any message passed to the tempate, along with the source code
-  ## file name and line number the debug message orignates from. 
+  ## any message passed to the template, along with the source code
+  ## file name and line number the debug message originates from. 
   ## To use, add this file 'dbgUtils.nim' to a project, and then
   ## import it into any Nim source code where a debug message is
   ## required. 
